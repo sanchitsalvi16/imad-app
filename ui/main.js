d412e72ae22f636ae8a97427b85289386d5cc72d
sanchit <36986617+sanchitsalvi16@users.noreply.github.com>
2018-03-03 13:08:55 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,7 +4,7 @@ var element=document.GetElementbyId('main-text'
 );
 element.innerHtml='new value';
 //move the image
-var img= document.getElementbyId('id');
+var img= document.getElementbyId('madi');
 img.onclick=function () {
     img.style.leftmargin='100px';
 }
